fdf64f7e025df5a0426fd10be0830853fcf8858f
Aswini Samantray <aswinisamantray.7149@gmail.com>
2023-07-23 19:55:41 +0530

    Developed the app little work left

---
+++ index.js
@@ -0,0 +1,35 @@
+const http=require('http');
+const fs=require('fs');
+const requests=require('requests');
+// e6017ae1ece368f466e3744bef784516 API KEY
+// http://api.openweathermap.org/geo/1.0/direct?q=Pune,IN&appid=e6017ae1ece368f466e3744bef784516
+
+function replaceVal(tempVal,orgVal){
+    console.log(tempVal)
+    console.log(orgVal[0].name)
+    console.log(orgVal[0].country)
+    let location=tempVal.replace('{%location%}',orgVal[0].name);
+    location=location.replace('{%country%}',orgVal[0].country);
+    return location;
+}
+const homeFile=fs.readFileSync('home.html','utf-8');
+console.log(homeFile);
+const server=http.createServer((req,res)=>{
+    if(req.url==='/'){
+        requests('http://api.openweathermap.org/geo/1.0/direct?q=Mumbai,IN&appid=e6017ae1ece368f466e3744bef784516')
+        .on('data',(chunk)=> {
+            const objData=JSON.parse(chunk);
+            const arrData=[objData];
+                const realTimedata= arrData.map((val)=>replaceVal(homeFile,val)).join("");
+                console.log(realTimedata);
+            })
+        .on('end',(err)=> {
+            if (err) return console.log('connection closed due to errors', err);
+                 console.log('end');
+            });
+    }
+})
+
+server.listen(8000,'127.0.0.1')
+
+
---
+++ script.js
@@ -0,0 +1,95 @@
+var days= ['Sun', 'Mon', 'Tues', 'Wednes', 'Thurs', 'Fri', 'Satur'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
+const apikey='e6017ae1ece368f466e3744bef784516';
+    var now=new Date();
+    var month=months[now.getMonth()].slice(0,3).toUpperCase();
+    var day=days[now.getDay()].slice(0,3).toUpperCase();
+    var minute=now.getMinutes();
+    if(minute<10){
+        minute='0'+minute;
+    }
+    var hours=now.getHours();
+    var bg=document.getElementById('container');
+    var date=document.getElementById('date');
+    var AIQ=document.getElementById('aiq');
+    var title=document.getElementById('title');
+    var city=document.getElementById('location');
+    var map=document.getElementById('icon');
+
+    var co=document.getElementById('co');
+    var no=document.getElementById('no');
+    var no2=document.getElementById('no2');
+    var o3=document.getElementById('o3');
+    var so2=document.getElementById('so2');
+    var nh3=document.getElementById('nh3');
+    if((hours>=18 && hours<=24)||(hours>=0 && hours<=5)){
+        bg.style.backgroundImage ='url("images/night.jpeg")'
+        date.style.color='white';
+        title.style.color='white';
+        city.style.color='white';
+        map.style.filter='hue-rotate(90deg)';
+    }
+    else{
+        bg.style.backgroundImage ='url("images/day.webp")'
+        date.style.color='black';
+        title.style.color='black';
+        city.style.color='black';
+    }
+    if(hours>=12 && hours<=24){
+        hours=hours-12;
+    }
+    if(hours<10){
+        hours='0'+hours;
+    }
+    var time=hours+':'+minute;
+    if(now.getHours()>=12 && now.getHours()<=24){
+        time=time+'PM';
+    }
+    else{
+        time=time+'AM';
+    }
+    document.getElementById('date').textContent=day+" | "+month+" "+now.getDate()+" | "+time;
+    console.log(day+" | "+month+" "+now.getDate()+" | "+time);
+    var lat=0;
+    var lon=0;
+
+    document.addEventListener('DOMContentLoaded',()=>{
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Faridabad,IN&appid=${apikey}`)
+        .then((res)=>res.json())
+        .then((data) => {
+          const location = data[0].name + ', ' + data[0].country;
+          console.log(location);
+          const locationElement = document.getElementById('location');
+          locationElement.textContent = location;
+          lat=data[0].lat;
+          lon=data[0].lon;
+          fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apikey}`)
+          .then((res)=>res.json())
+          .then((data)=>{
+            const AIQ=document.getElementById('aiq');
+            let air=data.list[0].main.aqi;
+            AIQ.textContent=air;
+            let component=data.list[0].components;
+            co.textContent="CO: "+component.co + " μg/m3";
+            no.textContent="NO: "+component.no + " μg/m3";
+            no2.textContent="NO2: "+component.no2 + " μg/m3";
+            nh3.textContent="NH3: "+component.nh3 + " μg/m3";
+            o3.textContent="O3: "+component.o3 + " μg/m3";
+            so2.textContent="SO2: "+component.so2 + " μg/m3";
+
+            const image=document.getElementById('weathercondn');
+            if(air<=5 && air>3){
+                image.src='images/air-pollution.png';
+                AIQ.style.color='red';
+            }
+            else if(air==3){
+                image.src='images/medium.png';
+                AIQ.style.color='yellow';
+            }
+            else{
+                image.src='images/low.png';
+                AIQ.style.color='green';
+            }
+          })
+        })
+    });
